2ce0839d39d8f21e170308b7977256a0b71efe25
Priyobrato Das <priyodas12@gmail.com>
2022-09-26 20:32:53 +0530

    auth with encoder

--- src/main/java/io/springboot/springbootsecurityv3/config/SpringSecurityConfig.java
+++ src/main/java/io/springboot/springbootsecurityv3/config/SpringSecurityConfig.java
@@ -1,18 +1,31 @@
 package io.springboot.springbootsecurityv3.config;
 
+import io.springboot.springbootsecurityv3.controller.ProductController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public static Logger logger= LoggerFactory.getLogger(SpringSecurityConfig.class);
+
+    @Autowired
+    private PasswordConfig passwordConfig;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        //super.configure(http);
         http.authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*")//api whitelisting
                 .permitAll()
@@ -21,4 +34,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
+    //to supply authentication details
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails abc= User.builder()
+                                    .username("abc")
+                                    .password(passwordConfig.getPasswordEncoder()
+                                                            .encode("abc123"))
+                                    .roles("DEV")
+                                    .build();
+        logger.info("encrypted password: {}, authorities: {}",abc.getPassword(),abc.getAuthorities());
+        return new InMemoryUserDetailsManager(abc);
+    }
+
+
 }
